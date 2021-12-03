a1a0a9ab2b4562b57291d17b140c7dc27360d5df
Adrianna <adriannam@workmail.com>
2021-12-03 10:43:49 -0500

    first commit of remade kanji-gate app.

---
+++ README.md
@@ -0,0 +1,2 @@
+https://app.kanjialive.com/api/docs  
+https://rapidapi.com/KanjiAlive/api/learn-to-read-and-write-japanese-kanji
---
+++ backend/routes/users.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/** Routes for users. */
+
+const jsonschema = require("jsonschema");
+
+const express = require("express");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const User = require("../models/user");
+
+const router = express.Router();
+
+/** GET /[username] => { user }
+ *
+ * Returns { username, firstName, lastName, isAdmin, jobs }
+ *   where jobs is { id, title, companyHandle, companyName, state }
+ *
+ * Authorization required: admin or same user-as-:username
+ **/
+
+router.get("/:username", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const user = await User.get(req.params.username);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[username]  =>  { deleted: username }
+ *
+ * Authorization required: admin or same-user-as-:username
+ **/
+
+router.delete("/:username", ensureCorrectUser, async function (req, res, next) {
+  try {
+    await User.remove(req.params.username);
+    return res.json({ deleted: req.params.username });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
